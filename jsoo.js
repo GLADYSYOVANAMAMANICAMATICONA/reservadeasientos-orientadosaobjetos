0e169285eab664fccbfaed8daa9a5b20681f652f
GLADYSYOVANAMAMANICAMATICONA <roxsy.lj@gmail.com>
2017-07-25 12:12:24 -0500

    avance de reservadeasientosorientadoaobjetos

---
+++ jsoo.js
@@ -0,0 +1,21 @@
+//creando objeto
+
+
+function Pasajeros (numasiento, nombre, apellido, dni){
+    this.numasiento = numasiento;//propiedadessss.esto no es funcionnnnnnnnnn.... 
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.dni = dni;
+function Bus(){
+	this.arrayDePasajeros = [];
+//clase... plantilla....de BusAsientos....funcion constructora
+	this.reservar = function(){
+	}//hace referencia al objeto actual, los parametros de entrada van a quedar asignadas a ese objeto BusAsientos.
+	this.listar = function(){
+	};//methodos de la clase...funcion, en el medio va codigos de instruccion.... son accionessss en verboooooooo
+	this.buscar = function(){
+	}//
+	this.cancelar = function(){
+	}
+}
+var arraydepasajeros = new arrayDePasajeros();
